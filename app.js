5e831e735048d6c2074d7b9bfc9e152b4b9df73b
John Kim <cloudiosx@gmail.com>
2021-10-05 17:12:07 -0400

    Complete Routing Project

--- app.js
+++ app.js
@@ -3,24 +3,73 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello from Express!");
+  res.send("Hello from Express!");
+});
 
-})
-const regex = /[/w+][^\/]/;
+const regex6 = /^(\/about\/foo)$/;
 
+app.get(regex6, (req, res) => {
+  res.status(404);
+  res.send("Something went wrong");
+});
 
-app.get(regex, (req, res) => {
-    // console.log("log1:", req.method);
-    // console.log("log2:", req.path);
-    // console.log("log3:", Math.ceil(Math.random()*9));
-    res.render("index", {method: req.method, path: req.path, randomNumber: Math.ceil(Math.random()*9)});
-})
+const regex5 = /(contact)$/;
+
+app.get(regex5, (req, res) => {
+  res.send("Contact");
+});
+
+const regex4 = /(bio)$/;
+
+app.get(regex4, (req, res) => {
+  res.send("Bio");
+});
+
+const regex3 = /^(\/capital-letters\/)/;
+
+app.get(regex3, (req, res) => {
+  const lowerLayer = req.path.split("/")[2];
+  res.send(lowerLayer.toUpperCase());
+});
+
+const regex2 = /(xyz)$/;
+
+app.get(regex2, (req, res) => {
+  res.send("That's all I wrote.");
+});
+
+const regex1 = /[/w+][^\/]/;
+
+app.get(regex1, (req, res) => {
+  // console.log("log1:", req.method);
+  // console.log("log2:", req.path);
+  // console.log("log3:", Math.ceil(Math.random()*9));
+  res.render("index", {
+    method: req.method,
+    path: req.path,
+    randomNumber: Math.ceil(Math.random() * 9),
+  });
+});
+
+app.post(regex1, (req, res) => {
+  // console.log("log1:", req.method);
+  // console.log("log2:", req.path);
+  // console.log("log3:", Math.ceil(Math.random()*9));
+  res.render("index", {
+    method: req.method,
+    path: req.path,
+    randomNumber: Math.ceil(Math.random() * 9),
+  });
+});
+
+// app.all("*", (req, res) => {
+//   res.statusCode("404");
+// });
 
 app.set("view engine", "pug");
-
 
 const port = 8081;
 
 app.listen(port, () => {
-    console.log(`Server is now listening on port: ${port}`);
-})
+  console.log(`Server is now listening on port: ${port}`);
+});
